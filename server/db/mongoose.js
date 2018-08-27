bab8ea93105d8b05beed59c11a5718259b245c4f
Andy Huynh <andy.k.huynh@gmail.com>
2018-08-26 21:09:00 -1000

    Add POST /todos route and refactore mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+//import mongoose from  'mongoose'
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {mongoose};
+//export {mongooseDb};
